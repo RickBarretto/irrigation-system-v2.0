7c23672d0e4942c7ddc5c6e651cbcf068a03c2f8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-05 22:06:11 -0300

    [matrix] fix ring counter

--- src/matrix/column_selector.v
+++ src/matrix/column_selector.v
@@ -1,15 +1,21 @@
 module column_selector (
-    column_0,
-    column_1,
-    column_2,
+    output col[2:0],
     
     input enable,
     input clock,
     input reset
 );
 
-    flipflop_d (column_0, clock, column_0, enable);
-    flipflop_d (column_1, clock, column_2, column_0);
-    flipflop_d (column_2, clock, reset, column_1);
+// flipflop_jk
+//     output reg q,
+//     output reg not_q,
+//
+//    input clock,
+//    input reset,
+//    input j, k
+
+    flipflop_jk (col[2], not_col2, clock, 1, col[0], not_col0);
+    flipflop_jk (col[1], not_col1, clock, 1, col[2], not_col2);
+    flipflop_jk (col[0], not_col0, clock, 1, col[1], not_col1);
 
 endmodule
