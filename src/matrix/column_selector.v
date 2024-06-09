dd909094206ea394fe94baa07b7278ca92af0d83
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-08 21:16:44 -0300

    [matrix] use flipflop D for the ring counter

--- src/matrix/column_selector.v
+++ src/matrix/column_selector.v
@@ -1,21 +1,12 @@
 module column_selector (
-    output col[2:0],
+    output [2:0]col,
     
-    input enable,
     input clock,
-    input reset
+	 input clear
 );
 
-// flipflop_jk
-//     output reg q,
-//     output reg not_q,
-//
-//    input clock,
-//    input reset,
-//    input j, k
-
-    flipflop_jk (col[2], not_col2, clock, 1, col[0], not_col0);
-    flipflop_jk (col[1], not_col1, clock, 1, col[2], not_col2);
-    flipflop_jk (col[0], not_col0, clock, 1, col[1], not_col1);
+    flipflop_d (col[2], clock, 1, clear, col[0]);
+    flipflop_d (col[1], clock, clear, 1, col[1]);
+    flipflop_d (col[0], clock, clear, 1, col[2]);
 
 endmodule
