f94431a0b0297725f7e1234d28b234c06e278a10
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-08 23:06:47 -0300

    [matrix] format file

--- src/matrix/irrigation_mode_decoder.v
+++ src/matrix/irrigation_mode_decoder.v
@@ -1,3 +1,6 @@
+
+// Helper modules
+
 module is_splinker (
     output out,
     input [1:0]data
@@ -5,6 +8,7 @@ module is_splinker (
     not (not_dripper, data[1]);
     and (out, not_dripper, data[0]);
 endmodule
+
 
 module is_dripper (
     output out,
@@ -14,24 +18,29 @@ module is_dripper (
     and (out, not_splinker, data[1]);
 endmodule
 
+
+
 //! LED matrix's decoder
 //! 
-//! This decoder takes an encoded data and generates the images to be displayed on the LED matrix of the CPLD Kit
+//! This decoder takes an encoded data and generates the needed images 
+//! to be displayed on the LED matrix of the CPLD Kit, when showing the irrigation system.
 //! 
 //! To reduce resources usage, the images have been choosen to be simetric on the Y-axis,
 //! what means that only three columns are needed for a 5-column display.
 module irrigation_mode_decoder (
-    output [6:0]col_2, //! Columns: 0 & 4
-    output [6:0]col_1, //! Columns: 1 & 3
-    output [6:0]col_0, //! Columns: 2
+    output [6:0] col_2, //! Columns: 0 & 4
+    output [6:0] col_1, //! Columns: 1 & 3
+    output [6:0] col_0, //! Columns: 2
 
     //! Quick explanation of the entries
+    //! --------------------------------
+    //! 
     //! Data Meaning
     //! ---- ---------------
     //! 01   Splinker output
     //! 10   Dripper output
 
-    input [1:0]data
+    input [1:0] data
 );
 
     // Helper function
