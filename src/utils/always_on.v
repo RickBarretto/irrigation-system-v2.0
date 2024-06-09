c407d5de7e3283c74c2ac289d76954ed37b375ef
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-08 23:36:49 -0300

    [utils] add documentation

--- src/utils/always_on.v
+++ src/utils/always_on.v
@@ -1,3 +1,7 @@
+//! Always on returns a positive signal, independent of the given entry.
+//!
+//! There is a property that says: A + A' = 0
+//! Since, 0 or 1 is always 1.
 module always_on (
 	output on, 
 	input a
