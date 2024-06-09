32a5bc5dfe8b838d6e75aab79a4bce8f755c459c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-08 23:47:31 -0300

    [utils] update flipflop JK to use clear and preset

--- src/utils/flipflop_jk.v
+++ src/utils/flipflop_jk.v
@@ -3,16 +3,25 @@ module flipflop_jk (
 	output reg not_q,
 
 	input clock,
-	input reset,
+	input clear,
+	input preset,
 	input j, k
 );
 
-	always @(posedge clock)
-		case ({j, k})
-			2'b00: q <= q;
-			2'b01: q <= 0;
-			2'b10: q <= 1;
-			2'b11: q <= ~q;
-		endcase
-	
+	always @(posedge clock or posedge clear or posedge preset) begin
+		if (clear)  
+			q <= 0;
+		else if (preset) 
+			q <= 1;
+		else
+			case ({j, k})
+				2'b00: q <= q;
+				2'b01: q <= 0;
+				2'b10: q <= 1;
+				2'b11: q <= ~q;
+			endcase
+	end
+
+	not (not_q, q);
+
 endmodule 
