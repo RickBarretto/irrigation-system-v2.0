ecf9adfb51d885a2f7735cb731208940ed2c3003
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-09 14:07:49 -0300

    [matrix\irrigation-decoder] implement the decoder logic

--- src/matrix/image_decoders/irrigation_mode_decoder.v
+++ src/matrix/image_decoders/irrigation_mode_decoder.v
@@ -28,9 +28,41 @@ module irrigation_mode_decoder (
     //!    * * *    * * * * *
     //!  * * * * *    * * *
 
-    // Helper function
-    always_on (on, data[1]);
+    // Helper wires
 
+    wire on, off;
+    wire dripper_mode_on;
+
+    always_on (on, splinker_mode_on);
+    not (off, on);
+    not (dripper_mode_on, splinker_mode_on);
+
+    // Colunn 2
+    pipe (col_2[6], on);                // Y = 1
+    pipe (col_2[5], dripper_mode_on);   // Y = S'
+    pipe (col_2[4], dripper_mode_on);   // Y = S'
+    pipe (col_2[3], on);                // Y = 1
+    pipe (col_2[2], splinker_mode_on);  // Y = S
+    pipe (col_2[1], splinker_mode_on);  // Y = S
+    pipe (col_2[0], dripper_mode_on);   // Y = S'
+
+    // Column 1
+    pipe (col_1[6], dripper_mode_on);   // Y = S'
+    pipe (col_1[5], on);                // Y = 1
+    pipe (col_1[4], splinker_mode_on);  // Y = S
+    pipe (col_1[3], splinker_mode_on);  // Y = S
+    pipe (col_1[2], splinker_mode_on);  // Y = S
+    pipe (col_1[1], off);               // Y = 0
+    pipe (col_1[0], off);               // Y = 0
+
+    // Column 0
+    pipe (col_0[6], dripper_mode_on);   // Y = S'
+    pipe (col_0[5], on);                // Y = 1
+    pipe (col_0[4], on);                // Y = 1
+    pipe (col_0[3], on);                // Y = 1
+    pipe (col_0[2], on);                // Y = 1
+    pipe (col_0[1], on);                // Y = 1
+    pipe (col_0[0], on);                // Y = 1
 
 
 endmodule
