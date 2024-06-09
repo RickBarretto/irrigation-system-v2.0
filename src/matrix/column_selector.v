d3a8ef4ba7b45856657e453d50ba0754af449d60
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-08 23:02:00 -0300

    [matrix] add documentation to the ring counter

--- src/matrix/column_selector.v
+++ src/matrix/column_selector.v
@@ -1,8 +1,13 @@
+//! Column Selector is a 3 bit Ring Counter
+//! 
+//! The reason why we just need 3 bits for a 5 columns lenght matrix
+//! is the fact of the images being mirrored, 
+//! so 2 of those columns are exactly the same.
 module column_selector (
     output [2:0]col,
     
     input clock,
-	 input clear
+	input clear
 );
 
     flipflop_d (col[2], clock, 1, clear, col[0]);
