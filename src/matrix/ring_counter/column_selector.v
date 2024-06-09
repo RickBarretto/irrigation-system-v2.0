fb76e0015460d4d439d1d1dea68e84e010695249
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-09 16:35:25 -0300

    [matrix\ring-counter] remove initial pulse

--- src/matrix/ring_counter/column_selector.v
+++ src/matrix/ring_counter/column_selector.v
@@ -15,15 +15,11 @@ endmodule
 //! The reason why we just need 3 bits for a 5 columns lenght matrix
 //! is the fact of the images being mirrored,
 //! so 2 of those columns are exactly the same.
-module column_selector (output [2:0] col, input clock, input pulse);
-
-    wire back;
-
-    or (back, pulse, col[0]);
+module column_selector (output [2:0] col, input clock);
 
     //                   Q     clock    D
     //                 ------  -----  ------
-    simple_flipflop_d D2(col[2], clock, back  );
+    simple_flipflop_d D2(col[2], clock, col[0]);
     simple_flipflop_d D1(col[1], clock, col[2]);
     simple_flipflop_d D0(col[0], clock, col[1]);
 
