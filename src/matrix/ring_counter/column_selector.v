0d8f5e197eb3c78d9bc4f9341152e0c817b59bd8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-10 11:12:28 -0300

    [matrix\ring-counter] invert logic of the pulse

--- src/matrix/ring_counter/column_selector.v
+++ src/matrix/ring_counter/column_selector.v
@@ -3,12 +3,14 @@
 //! The reason why we just need 3 bits for a 5 columns lenght matrix
 //! is the fact of the images being mirrored,
 //! so 2 of those columns are exactly the same.
-module column_selector (output [2:0] col, input clock);
+module column_selector (output [2:0] col, input clock, input pulse);
 
-    //                     Q     clock  set   reset    D
-    //                   ------- ----- ------ ------ --------
-    simple_flipflop_d D2(col[2], clock, pulse,     0, col[0]);
-    simple_flipflop_d D1(col[1], clock,     0, pulse, col[2]);
-    simple_flipflop_d D0(col[0], clock,     0, pulse, col[1]);
+    not (released, pulse);
+
+    //                     Q     clock     set      reset     D
+    //                   ------- ------ --------- --------- ------
+    simple_flipflop_d D2(col[2], clock, released,     0   , col[0]);
+    simple_flipflop_d D1(col[1], clock,     0   , released, col[2]);
+    simple_flipflop_d D0(col[0], clock,     0   , released, col[1]);
 
 endmodule
