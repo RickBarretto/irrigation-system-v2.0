48415bca7dff3198b8837731de75e57f4bbfe0e6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-10 09:11:31 -0300

    [matrix\ring-counter] trying to fix that thing

--- src/matrix/ring_counter/column_selector.v
+++ src/matrix/ring_counter/column_selector.v
@@ -1,10 +1,12 @@
 module simple_flipflop_d(
     output reg q,
 
+    input preset,
     input clock,
     input d
 );
-    always @(posedge clock) begin
+    always @(posedge clock, posedge preset) begin
+        if (preset) q <= preset;
         q <= d;
     end
 endmodule
@@ -17,10 +19,10 @@ endmodule
 //! so 2 of those columns are exactly the same.
 module column_selector (output [2:0] col, input clock);
 
-    //                   Q     clock    D
-    //                 ------  -----  ------
-    simple_flipflop_d D2(col[2], clock, col[0]);
-    simple_flipflop_d D1(col[1], clock, col[2]);
-    simple_flipflop_d D0(col[0], clock, col[1]);
+    //                     Q    preset clock    D
+    //                   ------ ------ -----  ------
+    simple_flipflop_d D2(col[2],  1,   clock, col[0]);
+    simple_flipflop_d D1(col[1],  0,   clock, col[2]);
+    simple_flipflop_d D0(col[0],  0,   clock, col[1]);
 
 endmodule
