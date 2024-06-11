13fff66cfddbd969ad4a2d5a272a818ed6e8eac3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-11 17:03:11 -0300

    [timer\decoder] update docs and comments to add A and F output

--- src/timer/display_decoder.v
+++ src/timer/display_decoder.v
@@ -41,11 +41,11 @@ module display_decoder(
     //!  8  1000  0 0 0 0 0 0 0
     //!  9  1001  0 0 0 0 1 0 0
     //!  .  1010  * * * * * * *
-    //!  .  1011  * * * * * * *
-    //!  E  1100  0 1 1 0 0 0 0
-    //!  .  1101  * * * * * * *
-    //!  r  1110  1 1 1 1 0 1 0
-    //!  o  1111  1 1 0 0 0 1 0
+    //!  R  1011  0 1 1 0 0 0 0
+    //!  r  1100  1 1 1 1 0 1 0
+    //!  o  1101  1 1 0 0 0 1 0
+    //!  A  1110  0 0 0 1 0 0 0
+    //!  F  1111  0 1 1 1 0 0 0
 
     // Negative Wires
     wire [3:0] negative;
@@ -57,47 +57,48 @@ module display_decoder(
 
     // Segments
 
-    // Segment A = A'BC'D' + A'B'C'D + AC
-    and (a_term_1, negative[3], data[2], negative[1], negative[0]);
-    and (a_term_2, negative[3], negative[2], negative[1], data[0]);
-    and (a_term_3, data[3], data[1]);
+    // Segment A = A'B'C'D + BC'D' + ABC'
+    and (a_term_1, );
+    and (a_term_2, );
+    and (a_term_3, );
     or  (a, a_term_1, a_term_2, a_term_3);
 
-    // Segment B = BC'D + BCD' + AB
-    and (b_term_1, data[2], negative[1], data[0]);
-    and (b_term_2, data[2], data[1], negative[0]);
-    and (b_term_3, data[3], data[2]);
-    or  (b, b_term_1, b_term_2, b_term_3);
+    // Segment B = ABC'D' + A'B'C'D + A'BCD' + ACD
+    and (b_term_1, );
+    and (b_term_2, );
+    and (b_term_3, );
+    and (b_term_4, );
+    or  (b, b_term_1, b_term_2, b_term_3, b_term_3);
 
-    // Segment C = ACD' + B'CD' + ABC'
-    and (c_term_1, data[3], data[1], negative[0]);
-    and (c_term_2, negative[2], data[1], negative[0]);
-    and (c_term_3, data[3], data[2], negative[1]);
+    // Segment C = ABC'D' + ACD + B'CD'
+    and (c_term_1, );
+    and (c_term_2, );
+    and (c_term_3, );
     or  (c, c_term_1, c_term_2, c_term_3);
 
-    // Segment D = A'BC'D' + A'B'C'D + A'BCD + ACD'
-    and (d_term_1, negative[3], data[2], negative[1], negative[0]);
-    and (d_term_2, negative[3], negative[2], negative[1], data[0]);
-    and (d_term_3, negative[3], data[2], data[1], data[0]);
-    and (d_term_4, data[3], data[1], negative[0]);
+    // Segment D = A'B'C'D + BC'D' + BCD + ACD'
+    and (d_term_1, );
+    and (d_term_2, );
+    and (d_term_3, );
+    and (d_term_4, );
     or  (d, d_term_1, d_term_2, d_term_3, d_term_4);
 
-    // Segment E = A'BC' + A'D + B'D
-    and (e_term_1, negative[3], data[2], negative[1]);
-    and (e_term_2, negative[3], data[0]);
-    and (e_term_3, negative[2], data[0]);
+    // Segment E = A'BC'D' + B'C'D + A'CD
+    and (e_term_1, );
+    and (e_term_2, );
+    and (e_term_3, );
     or  (e, e_term_1, e_term_2, e_term_3);
 
-    // Segment F = A'B'D + CD + AC + B'C
-    and (f_term_1, negative[3], negative[2], data[0]);
-    and (f_term_2, data[1], data[0]);
-    and (f_term_3, data[3], data[1]);
-    and (f_term_4, negative[2], data[1]);
+    // Segment F = A'CD + B'CD' + ABC' + A'B'D
+    and (f_term_1, );
+    and (f_term_2, );
+    and (f_term_3, );
+    and (f_term_4, );
     or  (f, f_term_1, f_term_2, f_term_3, f_term_4);
 
-    // Segment G =A'BCD + A'B'C'
-    and (g_term_1, negative[3], data[2], data[1], data[0]);
-    and (g_term_2, negative[3], negative[2], negative[1]);
+    // Segment G = A'BCD + A'B'C'
+    and (g_term_1, );
+    and (g_term_2, );
     or  (g, g_term_1, g_term_2);
 
 
