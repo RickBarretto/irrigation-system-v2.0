564a7cacf625fa73ee784899cacde02d9455842b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-10 21:41:05 -0300

    [timer] add prototype of counters

---
+++ src/timer/down_from_counters/down_from_5.v
@@ -0,0 +1,11 @@
+module down_from_5 (
+    output [3:0] count,
+
+    input [2:0] set,
+    input [2:0] reset,
+
+    input clock
+);
+
+
+endmodule
---
+++ src/timer/down_from_counters/down_from_9.v
@@ -0,0 +1,11 @@
+module down_from_9 (
+    output [4:0] count,
+
+    input [3:0] set,
+    input [3:0] reset,
+
+    input clock
+);
+
+
+endmodule
