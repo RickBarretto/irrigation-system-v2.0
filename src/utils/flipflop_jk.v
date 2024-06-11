e7e9ccdb23fc3bcff5cb9c3a778e6756629f6a75
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-10 21:31:32 -0300

    [utils] update flipflop JK's interface

--- src/utils/flipflop_jk.v
+++ src/utils/flipflop_jk.v
@@ -1,27 +1,23 @@
 module flipflop_jk (
 	output reg q,
-	output reg not_q,
 
 	input clock,
-	input clear,
-	input preset,
+	input set,
+	input reset,
+
 	input j, k
 );
 
-	always @(posedge clock or posedge clear or posedge preset) begin
-		if (clear)
-			q <= 0;
-		else if (preset)
-			q <= 1;
+	always @(posedge clock or posedge reset or posedge set) begin
+		if (reset) 		q <= 0;
+		else if (set)	q <= 1;
 		else
 			case ({j, k})
-				2'b00: q <= q;
-				2'b01: q <= 0;
-				2'b10: q <= 1;
-				2'b11: q <= ~q;
+				2'b00: 	q <= q;
+				2'b01: 	q <= 0;
+				2'b10: 	q <= 1;
+				2'b11: 	q <= ~q;
 			endcase
 	end
-
-	not (not_q, q);
 
 endmodule
