a48f9ed160ce14b99748072a65acdbffb03e7156
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-06-11 09:04:49 -0300

    [timer] invert the logic of the display's decoder

--- src/timer/display_decoder.v
+++ src/timer/display_decoder.v
@@ -57,13 +57,48 @@ module display_decoder(
 
     // Segments
 
-    // Segment A =
-    // Segment B =
-    // Segment C =
-    // Segment D =
-    // Segment E =
-    // Segment F =
-    // Segment G =
+    // Segment A = A'BC'D' + A'B'C'D + AC
+    and (a_term_1, negative[3], data[2], negative[1], negative[0]);
+    and (a_term_2, negative[3], negative[2], negative[1], data[0]);
+    and (a_term_3, data[3], data[1]);
+    or  (a, a_term_1, a_term_2, a_term_3);
+
+    // Segment B = BC'D + BCD' + AB
+    and (b_term_1, data[2], negative[1], data[0]);
+    and (b_term_2, data[2], data[1], negative[0]);
+    and (b_term_3, data[3], data[2]);
+    or  (b, b_term_1, b_term_2, b_term_3);
+
+    // Segment C = ACD' + B'CD' + ABC'
+    and (c_term_1, data[3], data[1], negative[0]);
+    and (c_term_2, negative[2], data[1], negative[0]);
+    and (c_term_3, data[3], data[2], negative[1]);
+    or  (c, c_term_1, c_term_2, c_term_3);
+
+    // Segment D = A'BC'D' + A'B'C'D + A'BCD + ACD'
+    and (d_term_1, negative[3], data[2], negative[1], negative[0]);
+    and (d_term_2, negative[3], negative[2], negative[1], data[0]);
+    and (d_term_3, negative[3], data[2], data[1], data[0]);
+    and (d_term_4, data[3], data[1], negative[0]);
+    or  (d, d_term_1, d_term_2, d_term_3, d_term_4);
+
+    // Segment E = A'BC' + A'D + B'D
+    and (e_term_1, negative[3], data[2], negative[1]);
+    and (e_term_2, negative[3], data[0]);
+    and (e_term_3, negative[2], data[0]);
+    or  (e, e_term_1, e_term_2, e_term_3);
+
+    // Segment F = A'B'D + CD + AC + B'C
+    and (f_term_1, negative[3], negative[2], data[0]);
+    and (f_term_2, data[1], data[0]);
+    and (f_term_3, data[3], data[1]);
+    and (f_term_4, negative[2], data[1]);
+    or  (f, f_term_1, f_term_2, f_term_3, f_term_4);
+
+    // Segment G =A'BCD + A'B'C'
+    and (g_term_1, negative[3], data[2], data[1], data[0]);
+    and (g_term_2, negative[3], negative[2], negative[1]);
+    or  (g, g_term_1, g_term_2);
 
 
 endmodule
